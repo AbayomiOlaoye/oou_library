c41421991492b974dd00e6fe7e3d636993c8f96e
Oluwatoyin Olaoye <mindprepopps@gmail.com>
2023-01-11 15:31:55 +0100

    Create Teacher class

--- student.rb
+++ student.rb
@@ -5,10 +5,9 @@ class Student < Person
   attr_reader :classroom, :age, :parent_permission
   attr_accessor :type
 
-  def initialize(classroom, name, age, parent_permission, type = 'Student')
+  def initialize(classroom, name, age, parent_permission)
     super(age, name, parent_permission)
     @classroom = classroom
-    @type = type
   end
 
   def play_hooky
