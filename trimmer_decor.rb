fc6ab688f91e71e3cbb5f2691129579d0d016f06
Oluwatoyin Olaoye <mindprepopps@gmail.com>
2023-01-12 13:15:27 +0100

    Implement code reviewer's suggestion

--- trimmer_decor.rb
+++ trimmer_decor.rb
@@ -3,6 +3,6 @@ require_relative './base_decorator'
 # TrimmerDecorator class to trim names to max of 10
 class TrimmerDecorator < Decorator
   def correct_name
-    @nameable.correct_name.strip.slice(0, 10) if @nameable.correct_name.length > 10
+    @nameable.correct_name.strip.slice(0, 9) if @nameable.correct_name.length > 10
   end
 end
