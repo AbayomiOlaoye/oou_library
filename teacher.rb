c0cd774f27cfefa76720baba2ca45d8731b4bb04
Oluwatoyin Olaoye <mindprepopps@gmail.com>
2023-01-14 21:11:56 +0100

    Create main.rb file

--- teacher.rb
+++ teacher.rb
@@ -2,11 +2,12 @@ require_relative './person'
 
 # top-level documentation comment for class Teacher.
 class Teacher < Person
-  attr_accessor :specialization
+  attr_accessor :specialization, :type
 
-  def initialize(specialization, age, name, parent_permission)
+  def initialize(specialization, age, name, parent_permission, type = 'Teacher')
     super(age, name, parent_permission)
     @specialization = specialization
+    @type = type
   end
 
   def can_use_services?
