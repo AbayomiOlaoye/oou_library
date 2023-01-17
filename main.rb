2a873cf50c4aa651debd0355b0f1628d1d6cab13
Oluwatoyin Olaoye <mindprepopps@gmail.com>
2023-01-17 13:35:30 +0100

    Implement refactoring on the principle of single-responsibility

--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 require_relative './app'
+require_relative './display_menu'
 
 def main
   app = App.new
@@ -6,21 +7,6 @@ def main
     display_menu
     choose_action(app)
   end
-end
-
-def display_menu
-  puts "\nWelcome to School Library App!"
-  puts "\nPlease choose an option by entering a number:"
-  options = [
-    '1 - List all books',
-    '2 - List all people',
-    '3 - Create a person',
-    '4 - Create a book',
-    '5 - Create a rental',
-    '6 - List all rentals for a given person id',
-    '7 - Exit'
-  ]
-  puts options
 end
 
 def choose_action(app)
